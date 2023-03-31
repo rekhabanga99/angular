bdd0d17f6e07eb3a96b1d1b9770252137d8297b9
HS <rekha.banga@habilelabs.io>
2023-03-31 14:26:32 +0530

    routing

--- src/app/shared/app-routing.module.ts
+++ src/app/shared/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeeComponent } from '../add-employee/add-employee.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/add', pathMatch: 'full' },
+  { path: '', component: PageNotFoundComponent, pathMatch: 'full' },
+  { path: 'add', component: AddEmployeeComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
